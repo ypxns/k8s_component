1ea113c8b77b4ab51ee92e6cbeeba75605861188
ablertyang <ablertyang@tencent.com>
2021-04-21 17:02:33 +0800

    add mycnat-controller

---
+++ mycnat-controller/go.mod
@@ -0,0 +1,20 @@
+// This is a generated file. Do not edit directly.
+
+module k8s.io/sample-controller
+
+go 1.16
+
+require (
+	k8s.io/api v0.0.0-20210411031832-6eed676bc189
+	k8s.io/apimachinery v0.0.0-20210411031630-d8cdd62f18c3
+	k8s.io/client-go v0.0.0-20210411032117-0bb6464b1348
+	k8s.io/code-generator v0.0.0-20210411031433-bdc239664504
+	k8s.io/klog/v2 v2.8.0
+)
+
+replace (
+	k8s.io/api => k8s.io/api v0.0.0-20210411031832-6eed676bc189
+	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20210411031630-d8cdd62f18c3
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20210411032117-0bb6464b1348
+	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20210411031433-bdc239664504
+)
---
+++ mycnat-controller/pkg/apis/cnat/v1alpha1/types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Phase
+const (
+	PhasePending = "PENDING"
+	PhaseRunning = "RUNNING"
+	PhaseDone    = "DONE"
+)
+
+// At is a specification for a At resource
+type At struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AtSpec   `json:"spec"`
+	Status AtStatus `json:"status"`
+}
+
+// AtSpec is the spec for a At resource
+type AtSpec struct {
+	Schedule string `json:"schedule,omitempty"`
+	Command  string `json:"command,omitempty"`
+}
+
+// AtStatus is the status for a At resource
+type AtStatus struct {
+	Phase string `json:"phase,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// AtList is a list of At resources
+type AtList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []At `json:"items"`
+}
